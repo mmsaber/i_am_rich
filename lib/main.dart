33ce14e1d1098f05e5816a96367d595f92cfb14e
mmsaber <mm.saber91@gmail.com>
2019-11-16 16:47:26 +0300

    checking SCM commit

--- lib/main.dart
+++ lib/main.dart
@@ -53,6 +53,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
+      //this is a test for deployment
       _counter++;
     });
   }
